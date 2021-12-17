d38935236c516345ad34ccfa7542cbd1e7970dd1
cody askew <codyaskew9@gmail.com>
2021-12-17 14:36:37 -0700

    fixed test mvp still not passing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,39 +1,68 @@
-import React from "react";
-import StyledPage from "./Style.js";
-import { Link } from "react-router-dom";
-
-const HomePage = () => {
-  return (
-    <StyledPage>
-      <div className="HeaderContainer">
-        <div className="Logo">
-          <h1>Lambda Eats</h1>
-        </div>
-        <div className="Navigation">
-          <Link to="/">Home</Link>
-          <Link id="order-pizza" to="/pizza">
-            Order a Pizza!
-          </Link>
-        </div>
-      </div>
-      <div className="TextContainer">
-        <div className="TextTitle">
-          <h2>
-            {" "}
-            Simply, the best pizzas!
-            <br />
-            -Gordon Ramsey{" "}
-          </h2>
-        </div>
-        <div className="TextParagraph">
-          <p>
-            Pizza from Lambda with love.
-            <br /> Every pizza is crafted by our chef Carlos <br /> Please click
-            the <span>Order Now</span> button to place your order now.
-          </p>
-        </div>
-      </div>
-    </StyledPage>
-  );
-};
-export default HomePage;
+describe("Form Test", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/Pizza");
+    });
+  
+    // Declaring testing units/parameters
+    const nameInput = () => cy.get('input[name="name"]').should("exist");
+    const specialInstructionsInput = () =>
+      cy.get('input[name="siInput"]').should("exist");
+    const pepperoniCheck = () =>
+      cy.get('input[name="Pepperoni"]').should("exist");
+    const hamCheck = () => cy.get('input[name="Ham"]').should("exist");
+    const pineappleCheck = () =>
+      cy.get('input[name="Pineapple"]').should("exist");
+    const mushroomsCheck = () =>
+      cy.get('input[name="Mushrooms"]').should("exist");
+    const baconCheck = () => cy.get('input[name="Bacon"]').should("exist");
+    const peppersCheck = () => cy.get('input[name="Peppers"]').should("exist");
+    const onionsCheck = () => cy.get('input[name="Onions"]').should("exist");
+    const extracheeseCheck = () =>
+      cy.get('input[name="ExtraCheese"]').should("exist");
+    const buttonSubmit = () => cy.get("button").should("exist");
+  
+    // Start exist testing
+    it("Input Exists", () => {
+      //Start Assertion(s)
+      nameInput().should("exist");
+      specialInstructionsInput().should("exist");
+      pepperoniCheck().should("exist");
+      hamCheck().should("exist");
+      pineappleCheck().should("exist");
+      mushroomsCheck().should("exist");
+      baconCheck().should("exist");
+      peppersCheck().should("exist");
+      onionsCheck().should("exist");
+      extracheeseCheck().should("exist");
+      buttonSubmit().should("exist");
+    });
+  
+    // Start type testing
+    it("Type in Name Field", () => {
+      nameInput().type("Carlos");
+      nameInput().should("have.value", "Carlos");
+      specialInstructionsInput().type("Please call instead of knocking on door");
+      specialInstructionsInput().should(
+        "have.value",
+        "Please call instead of knocking on door"
+      );
+    });
+  
+    //Start Checkbox testing
+    it("Check Toppings Checkboxes", () => {
+      pepperoniCheck().check();
+      hamCheck().check();
+      pineappleCheck().check();
+      mushroomsCheck().check();
+      baconCheck().check();
+      peppersCheck().check();
+      onionsCheck().check();
+      extracheeseCheck().check();
+    });
+  
+    //Submit Button Test
+    it("Submit Form", () => {
+      buttonSubmit().click();
+    });
+  });
+  
